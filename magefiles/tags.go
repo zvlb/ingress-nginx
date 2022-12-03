0aa498704fe9b407773307a787663d56a330cb46
James Strong <strong.james.e@gmail.com>
2022-12-03 13:17:28 -0500

    semver checking
    Signed-off-by: James Strong <strong.james.e@gmail.com>

--- magefiles/tags.go
+++ magefiles/tags.go
@@ -4,19 +4,22 @@ package main
 
 import (
 	"fmt"
+	semver "github.com/blang/semver/v4"
 	"github.com/magefile/mage/mg"
-
 	"github.com/magefile/mage/sh"
 	"os"
+	"strings"
 )
 
 func getTag() (string, error) {
+	var tag string
 	dat, err := os.ReadFile("TAG")
 	if err != nil {
 		return "", err
 	}
-
-	return string(dat), nil
+	//remove newline
+	tag = strings.Replace(string(dat), "\n", "", -1)
+	return tag, nil
 }
 
 // Generate Release Notes
@@ -34,20 +37,58 @@ func (Tag) Nginx() {
 	fmt.Printf("%v", tag)
 }
 
-func bump(tag string) {
-	dat, err := os.ReadFile("TAG")
+func checkSemVer(currentTag, newTag string) bool {
+	cTag, err := semver.Make(currentTag[1:])
 	if err != nil {
-		fmt.Printf("Error %v", err)
+		fmt.Printf("%v Not a valid Semver\n", newTag)
+		return false
 	}
-	fmt.Printf("Updating Tag %v to %v\n", string(dat), tag)
-	os.WriteFile("TAG", []byte(tag), 0666)
+	nTag, err := semver.Make(newTag[1:])
+	if err != nil {
+		fmt.Printf("%v Not a valid Semver\n", newTag)
+		return false
+	}
+
+	err = nTag.Validate()
+	if err != nil {
+		fmt.Printf("%v Not a valid Semver\n", newTag)
+		return false
+	}
+
+	//The result will be
+	//0 if newTag == currentTag
+	//-1 if newTag< currentTag
+	//+1 if newTag > currentTag.
+	comp := nTag.Compare(cTag)
+	if comp <= 0 {
+		fmt.Printf("SemVer:%v is not an update\n", newTag)
+		return false
+	}
+	return true
+}
+
+func bump(currentTag, newTag string) {
+	//check if semver is valid
+	if !checkSemVer(currentTag, newTag) {
+		fmt.Printf("ERROR: Semver is not valid %v\n", newTag)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Updating Tag %v to %v\n", currentTag, newTag)
+	err := os.WriteFile("TAG", []byte(newTag), 0666)
 	if err != nil {
 		fmt.Printf("Error %v", err)
+		os.Exit(1)
 	}
 }
 
-func (Tag) Bump(tag string) {
-	bump(tag)
+func (Tag) Bump(newTag string) {
+	currentTag, err := os.ReadFile("TAG")
+	if err != nil {
+		fmt.Printf("Error %v", err)
+	}
+
+	bump(string(currentTag), newTag)
 }
 func (Tag) Git() {
 	getGitTag()
