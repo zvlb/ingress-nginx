ac9caf7e08d89c1e19093340a87d10dc645170f0
James Strong <strong.james.e@gmail.com>
2022-12-04 17:29:05 -0500

    get all commits between two tags
    Signed-off-by: James Strong <strong.james.e@gmail.com>

--- magefiles/release-notes.go
+++ magefiles/release-notes.go
@@ -1,3 +1,29 @@
 //go:build mage
 
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// LatestCommitLogs Retrieves the commit log between the latest two controller versions.
+func (Release) LatestCommitLogs() {
+	commitsBetweenTags()
+}
+
+func commitsBetweenTags() {
+	tags := getAllControllerTags()
+	Info("Getting Commits between %v and %v", tags[0], tags[1])
+	commitLog, err := git("log", fmt.Sprintf("%v..%v", tags[1], tags[0]))
+	if commitLog == "" {
+		Warning("All Controller Tags is empty")
+	}
+	CheckIfError(err, "Retrieving Commit log")
+	temp := strings.Split(commitLog, "\n")
+	Info("There are %v commits", len(commitLog))
+	Info("Commits between %v..%v", tags[1], tags[0])
+	for i, s := range temp {
+		Info("#%v Version %v", i, s)
+	}
+}
